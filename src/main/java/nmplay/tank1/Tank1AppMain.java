c83250ba56a9e2beb25db58386fe8967eab7d36d
Kazuaki Nishizawa <Kazuaki.Nishizawa@gmail.com>
2015-02-12 07:32:21 +0900

    revise main.java

--- src/main/java/nmplay/tank1/Tank1AppMain.java
+++ src/main/java/nmplay/tank1/Tank1AppMain.java
@@ -6,30 +6,39 @@ import com.github.nkzawa.emitter.Emitter;
 import com.github.nkzawa.socketio.client.IO;
 import com.github.nkzawa.socketio.client.Socket;
 
+/**
+ * Tank1 main.
+ *
+ * @author LightSpeedC
+ */
 public class Tank1AppMain {
 
 	/**
 	 * main.
 	 *
 	 * @param args
+	 *            String...
 	 * @throws URISyntaxException
+	 *             Exception
 	 */
-	public static void main(String[] args) throws URISyntaxException {
+	public static void main(final String... args) throws URISyntaxException {
 		System.out.println("hello\n");
+
 		// final Socket socket = IO.socket("http://localhost:3000");
 		final Socket socket = IO.socket("https://tank1.herokuapp.com");
+		final TankSession session = new TankSession();
+
 		socket.connect();
 		socket.on("connect", new Emitter.Listener() {
 			public void call(Object... args) {
 				System.out.println("connect");
-				socket.emit("first", "{\"first\":\"message from client.java\"}");
+				socket.emit("get messages", "{\"since\":"
+						+ session.last_received_at + "}");
 			}
 		});
-		socket.on("first", new Emitter.Listener() {
+		socket.on("response messages", new Emitter.Listener() {
 			public void call(Object... args) {
-				System.out.println("first: " + args[0]);
-				socket.emit("other event",
-						"{\"other\":\"event from client.java\"}");
+				System.out.println("response messages: " + args[0]);
 			}
 		});
 		socket.on("disconnect", new Emitter.Listener() {
@@ -40,7 +49,12 @@ public class Tank1AppMain {
 		socket.on("message", new Emitter.Listener() {
 			public void call(Object... args) {
 				System.out.println("message: " + args[0]);
+				// session.last_received_at = ...
 			}
 		});
+	}
+
+	static class TankSession {
+		int last_received_at = 0;
 	}
 }
